1236eab9cd331f7599f4ebbb2c994a70f9eaab94
szsascha <43013380+szsascha@users.noreply.github.com>
2023-08-30 12:47:30 +0200

    Deployment optimizations

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+docker compose build
+
 for img in $(docker-compose config | awk '{if ($1 == "image:") print $2;}'); do
   images="$images $img"
 done
@@ -8,4 +10,3 @@ echo $images
 
 docker image save $images | docker image load
 docker-compose up --force-recreate -d
-docker-compose logs -f
